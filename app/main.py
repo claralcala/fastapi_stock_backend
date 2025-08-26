c4fef0d3bfeddc965154a16a4071c53a98b6eb37
Clara Alcalá García <calcgar312@iescarrillo.es>
2025-08-26 11:13:05 +0200

    Update main.py
    Añadido corsmiddleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routers import stock_router
+from fastapi.middleware.cors import CORSMiddleware
 
 # Crear tablas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Stock API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],        
+)
 
 app.include_router(stock_router.router)
